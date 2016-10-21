fb1f45387cd5d5ded8b19a9ace9fe14a04dad5f7
Christy Perez <christy@linux.vnet.ibm.com>
2016-10-21 10:28:01 -0500

    Make valid osArches a map with a struct key
    To avoid string formatting
    
    Signed-off-by: Christy Perez <christy@linux.vnet.ibm.com>

--- cli/command/manifest/util.go
+++ cli/command/manifest/util.go
@@ -1,44 +1,48 @@
 package manifest
 
-import "fmt"
-
 // list of valid os/arch values (see "Optional Environment Variables" section
 // of https://golang.org/doc/install/source
 // Added linux/s390x as we know System z support already exists
 
-var validOSArch = map[string]bool{
-	"darwin/386":      true,
-	"darwin/amd64":    true,
-	"darwin/arm":      true,
-	"darwin/arm64":    true,
-	"dragonfly/amd64": true,
-	"freebsd/386":     true,
-	"freebsd/amd64":   true,
-	"freebsd/arm":     true,
-	"linux/386":       true,
-	"linux/amd64":     true,
-	"linux/arm":       true,
-	"linux/arm64":     true,
-	"linux/ppc64":     true,
-	"linux/ppc64le":   true,
-	"linux/mips64":    true,
-	"linux/mips64le":  true,
-	"linux/s390x":     true,
-	"netbsd/386":      true,
-	"netbsd/amd64":    true,
-	"netbsd/arm":      true,
-	"openbsd/386":     true,
-	"openbsd/amd64":   true,
-	"openbsd/arm":     true,
-	"plan9/386":       true,
-	"plan9/amd64":     true,
-	"solaris/amd64":   true,
-	"windows/386":     true,
-	"windows/amd64":   true,
+type osArch struct {
+	os   string
+	arch string
+}
+
+//Remove any unsupported os/arch combo
+var validOSArches = map[osArch]bool{
+	osArch{os: "darwin", arch: "386"}:      true,
+	osArch{os: "darwin", arch: "amd64"}:    true,
+	osArch{os: "darwin", arch: "arm"}:      true,
+	osArch{os: "darwin", arch: "arm64"}:    true,
+	osArch{os: "dragonfly", arch: "amd64"}: true,
+	osArch{os: "freebsd", arch: "386"}:     true,
+	osArch{os: "freebsd", arch: "amd64"}:   true,
+	osArch{os: "freebsd", arch: "arm"}:     true,
+	osArch{os: "linux", arch: "386"}:       true,
+	osArch{os: "linux", arch: "amd64"}:     true,
+	osArch{os: "linux", arch: "arm"}:       true,
+	osArch{os: "linux", arch: "arm64"}:     true,
+	osArch{os: "linux", arch: "ppc64"}:     true,
+	osArch{os: "linux", arch: "ppc64le"}:   true,
+	osArch{os: "linux", arch: "mips64"}:    true,
+	osArch{os: "linux", arch: "mips64le"}:  true,
+	osArch{os: "linux", arch: "s390x"}:     true,
+	osArch{os: "netbsd", arch: "386"}:      true,
+	osArch{os: "netbsd", arch: "amd64"}:    true,
+	osArch{os: "netbsd", arch: "arm"}:      true,
+	osArch{os: "openbsd", arch: "386"}:     true,
+	osArch{os: "openbsd", arch: "amd64"}:   true,
+	osArch{os: "openbsd", arch: "arm"}:     true,
+	osArch{os: "plan9", arch: "386"}:       true,
+	osArch{os: "plan9", arch: "amd64"}:     true,
+	osArch{os: "solaris", arch: "amd64"}:   true,
+	osArch{os: "windows", arch: "386"}:     true,
+	osArch{os: "windows", arch: "amd64"}:   true,
 }
 
 func isValidOSArch(os string, arch string) bool {
-	osarch := fmt.Sprintf("%s/%s", os, arch)
-	_, ok := validOSArch[osarch]
+	// check for existence of this combo
+	_, ok := validOSArches[osArch{os, arch}]
 	return ok
 }
