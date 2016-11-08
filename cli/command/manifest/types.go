9b6f1326aa03795b694580503b08ab11576ce900
Christy Perez <christy@linux.vnet.ibm.com>
2016-11-08 11:34:13 -0600

    Rework for annotating
    Signed-off-by: Christy Perez <christy@linux.vnet.ibm.com>

--- cli/command/manifest/types.go
+++ cli/command/manifest/types.go
@@ -24,20 +24,20 @@ func (f fallbackError) Error() string {
 
 // ImgManifestInspect contains info to output for a manifest object.
 type ImgManifestInspect struct {
-	Size            int64
-	MediaType       string
-	Tag             string
-	Digest          string
-	RepoTags        []string
-	Comment         string
-	Created         string
-	ContainerConfig *containerTypes.Config
-	DockerVersion   string
-	Author          string
-	Config          *containerTypes.Config
-	Architecture    string
-	Os              string
-	Layers          []string
-	Platform        manifestlist.PlatformSpec
-	CanonicalJSON   []byte
+	Size            int64                     `json:size`
+	MediaType       string                    `json:media_type`
+	Tag             string                    `json:tag`
+	Digest          string                    `json:digest`
+	RepoTags        []string                  `json:repotags`
+	Comment         string                    `json:comment`
+	Created         string                    `json:created`
+	ContainerConfig *containerTypes.Config    `json:container_config`
+	DockerVersion   string                    `json:docker_version`
+	Author          string                    `json:author`
+	Config          *containerTypes.Config    `json:config`
+	Architecture    string                    `json:architecture`
+	Os              string                    `json:os`
+	Layers          []string                  `json:layers`
+	Platform        manifestlist.PlatformSpec `json:platform`
+	CanonicalJSON   []byte                    `json:canonical_json`
 }
