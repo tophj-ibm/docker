a55c1c89bde28e9f98ab27d144914ec412e1e206
Christy Perez <christy@linux.vnet.ibm.com>
2016-11-28 11:23:41 -0600

    Add other options on annotate
    Signed-off-by: Christy Perez <christy@linux.vnet.ibm.com>

--- cli/command/manifest/annotate.go
+++ cli/command/manifest/annotate.go
@@ -12,6 +12,8 @@ import (
 type annotateOptions struct {
 	remote      string
 	variant     string
+	os          string
+	arch        string
 	cpuFeatures []string
 	osFeatures  []string
 }
@@ -33,11 +35,11 @@ func newAnnotateCommand(dockerCli *command.DockerCli) *cobra.Command {
 	flags := cmd.Flags()
 
 	// @TODO: Should we do any validation? We can't have an exhaustive list
-	// of features, but at least check for only a csv of alpha-chars?
-	flags.StringSliceVarP(&opts.cpuFeatures, "cpuFeatures", "c", []string{}, "Add feature metadata to a manifest before pushing it.")
-	// @TODO: Maybe no shorthand? These are bad =D
-	flags.StringSliceVarP(&opts.osFeatures, "osFeatures", "o", []string{}, "Add feature metadata to a manifest before pushing it.")
-	flags.StringVarP(&opts.variant, "variant", "v", "", "Add arch variant to a manifest before pushing it.")
+	flags.StringVar(&opts.os, "os", "", "Add ios info to a manifest before pushing it.")
+	flags.StringVar(&opts.arch, "arch", "", "Add arch info to a manifest before pushing it.")
+	flags.StringSliceVar(&opts.cpuFeatures, "cpuFeatures", []string{}, "Add feature info to a manifest before pushing it.")
+	flags.StringSliceVar(&opts.osFeatures, "osFeatures", []string{}, "Add feature info to a manifest before pushing it.")
+	flags.StringVar(&opts.variant, "variant", "", "Add arch variant to a manifest before pushing it.")
 
 	command.AddTrustedFlags(flags, true)
 
@@ -71,8 +73,22 @@ func runManifestAnnotate(dockerCli *command.DockerCli, opts annotateOptions) err
 	}
 
 	// Update the mf
-	// @TODO: Verification? Move the one from create to here?
 	// @TODO: Prevent duplicates
+	// validate os/arch input
+	/*if opts.arch == "" || opts.os == "" {
+		return fmt.Errorf("You must specify an arch and os.")
+	}
+	if !isValidOSArch(opts.os, opts.arch) {
+		return fmt.Errorf("Manifest entry for image %s has unsupported os/arch combination: %s/%s", opts.remote, opts.os, opts.arch)
+	}*/
+	if opts.os != "" {
+		newMf.Os = opts.os
+		newMf.Platform.OS = opts.os
+	}
+	if opts.arch != "" {
+		newMf.Architecture = opts.arch
+		newMf.Platform.Architecture = opts.arch
+	}
 	if len(opts.cpuFeatures) > 0 {
 		newMf.Platform.Features = append(mf.Platform.Features, opts.cpuFeatures...)
 	}
@@ -82,6 +98,8 @@ func runManifestAnnotate(dockerCli *command.DockerCli, opts annotateOptions) err
 	if opts.variant != "" {
 		newMf.Platform.Variant = opts.variant
 	}
+
+	fmt.Printf("New Manifest: %s\n", newMf)
 
 	if err := updateMfFile(newMf); err != nil {
 		return err
